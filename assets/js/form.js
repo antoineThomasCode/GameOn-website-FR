b06ac0916fadc57fba08b292edbf311caa660059
antoineThomasCode <73194840+antoineThomasCode@users.noreply.github.com>
2021-12-17 17:45:28 +0100

    modified elements for making presentation

--- assets/js/form.js
+++ assets/js/form.js
@@ -22,31 +22,38 @@ function validate(){
     document.querySelectorAll('.error--bg').forEach(error => error.classList.remove('error--bg'));
 
     if(!nameIsCorrect(firstName.value)){
-        displayErrorMessage(firstName, 'Veuillez entrer 2 caractères ou plus pour le prénom.')
+        displayErrorMessage(firstName,
+             'Veuillez entrer 2 caractères ou plus pour le prénom.')
     }
     if(!nameIsCorrect(lastName.value)){
-        displayErrorMessage(lastName, 'Veuillez entrer 2 caractères ou plus pour le nom.')
+        displayErrorMessage(lastName, 
+            'Veuillez entrer 2 caractères ou plus pour le nom.')
     }
     if(!emailIsCorrect(email.value)){
-        displayErrorMessage(email, 'Veuillez entrer une adresse mail valide.')
+        displayErrorMessage(email, 
+            'Veuillez entrer une adresse mail valide.')
     }
     if(!checkAge(age.value)){
-        displayErrorMessage(age, 'Vous devez avoir au moins 18 ans.')
+        displayErrorMessage(age, 
+            'Vous devez avoir au moins 18 ans.')
         isErrors = true; 
     }
     if(!isInInterval(quantity.value)){
-        displayErrorMessage(quantity, 'Veuillez entrer une valeur comprise entre 0 et 99.')
+        displayErrorMessage(quantity, 
+            'Veuillez entrer une valeur comprise entre 0 et 99.')
     }
     if(!atLeastOneCheck(location)){
         isErrors = true;
         location[0].parentNode.classList.add('error--bg');
-        document.getElementsByClassName("formData")[5].insertAdjacentElement('afterend', createErrorMessage('Veuillez sélctionner une ville'));
+        document.getElementsByClassName("formData")[5].insertAdjacentElement('afterend',
+         createErrorMessage('Veuillez sélctionner une ville'));
         
     }
     if(!checkboxChecked(cgv)){
         isErrors = true;
         document.getElementsByClassName("checkbox2-label")[0].children[0].classList.add('error--bg');
-        document.getElementsByClassName("checkbox2-label")[0].insertAdjacentElement('afterend', createErrorMessage('Vous devez accepter les termes et conditions.'));
+        document.getElementsByClassName("checkbox2-label")[0].insertAdjacentElement('afterend',
+         createErrorMessage('Vous devez accepter les termes et conditions.'));
         
     }
     if(isErrors === true){
@@ -60,11 +67,19 @@ function validate(){
         previousparticipations : quantity.value,
         location: location.value
         };
-
+        const containerPresentation = document.createElement('div')
+        const object = document.createElement('img')
+        const img1 = document.getElementById('img1')
+        const img2 = document.getElementById('img2')
+        img1.style.display = 'none';
+        img2.style.display = 'none'
+        object.setAttribute("src", "../assets/img/validation_ok.JPG")
         formReserve.style.display = 'none';
         modalBody.classList.add('message-sended');
-        validationMessage.innerHTML='Merci, votre formulaire a bien été envoyé !';
+        validationMessage.innerHTML='Une fois les élements validés, cette modal est créée et les éléments sont envoyés au Backend';
         modalBody.append(validationMessage);
+        modalBody.appendChild(containerPresentation)
+        containerPresentation.appendChild(object)
         buttonClose.classList.add('button','button:hover','button-close');
         buttonClose.innerHTML = "Fermer";
         modalBody.appendChild(buttonClose);
